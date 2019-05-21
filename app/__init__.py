d7b52a851ce17876ee7ddccae20d5dcda5bd5c13
Iain Hunter <iain.hunter@outlook.com>
2019-05-21 23:59:04 +0100

    Add swagger (#1)
    * Work on web page
    
    * Working swagger

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,15 @@ import os
 
 from logging.handlers import RotatingFileHandler
 
+from flasgger import Swagger
 from flask import Flask
 
 # Update with app specific versions as needed
 APP_NAME = 'FLASK-BOOTSTRAP'
 APP_VERSION = 'v1'
+
+# Init dependencies
+swagger = Swagger()
 
 
 def bootstrap_app() -> Flask:
@@ -19,6 +23,7 @@ def bootstrap_app() -> Flask:
     app.logger.info('Mapfish-PY Starting Up, Environment = {0}'.format(get_current_environment()))
 
     register_flask_blueprints(app)
+    init_swagger_docs(app)
 
     return app
 
@@ -59,5 +64,16 @@ def register_flask_blueprints(app: Flask):
     from app.api import api as api_bp
     app.register_blueprint(api_bp, url_prefix=f'/api/{APP_VERSION}')
 
-    #from app.web import main as main_bp
-    #app.register_blueprint(main_bp)
+    # Web blueprint for html pages
+    from app.web import main as main_bp
+    app.register_blueprint(main_bp)
+
+
+def init_swagger_docs(app: Flask):
+    """ Initialise the swagger API docs using the flasgger lib.  Docs are available at /apidocs """
+    app.config['SWAGGER'] = {
+        'title': f'{APP_NAME} API',
+        'uiversion': 3,
+        "version": f"{APP_VERSION}"
+    }
+    swagger.init_app(app)
---
+++ app/web/home_controller.py
@@ -0,0 +1,13 @@
+from flask import render_template
+
+from app import APP_NAME
+from app.web import main
+
+
+@main.route('/')
+def home_page():
+    """
+    Controller to launch simple home page.  While flask can host SPA's better to host SPA separately.  Alternatively
+    you can always create apps with flask templates and Jinja :)
+    """
+    return render_template('home.html', app_name=APP_NAME)
