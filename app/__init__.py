b356d89508c04785f0ab494b33ff2cbc9ba41c11
Iain Hunter <iain.hunter@outlook.com>
2019-05-26 16:19:17 +0100

    Correct app name

--- app/__init__.py
+++ app/__init__.py
@@ -22,7 +22,7 @@ def bootstrap_app() -> Flask:
 
     initialise_logger(app)
     app.logger.info(
-        "Mapfish-PY Starting Up, Environment = {0}".format(get_current_environment())
+       f"{APP_NAME} Starting Up, Environment = {get_current_environment()}"
     )
 
     register_flask_blueprints(app)
