ed8e2665977e168fb5cda2f4ae8ba93abb0eb62f
Iain Hunter <iain.hunter@outlook.com>
2019-05-26 17:06:59 +0100

    Setup sqlite (#10)
    * Work on setup
    
    * Work on migrations
    
    * Correct app name
    
    * init migrations
    
    * Working migrations
    
    * Fixup linting errors

---
+++ app/models/database/user.py
@@ -0,0 +1,15 @@
+from app import db
+
+
+class User(db.Model):
+    """ Models the users table """
+
+    __tablename__ = "users"
+
+    id = db.Column(db.BigInteger, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return f"<User {self.username}>"
