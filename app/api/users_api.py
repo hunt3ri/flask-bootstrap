65bb7a1150ff87adb5ab1ac7a73917386df4172f
Iain Hunter <iain.hunter@outlook.com>
2019-05-30 17:27:11 +0100

    Add JWT session tokens (#14)
    * Add secret key
    
    * Working session token
    
    * Run black

--- app/api/users_api.py
+++ app/api/users_api.py
@@ -2,7 +2,7 @@ from flask import jsonify, request, current_app
 
 from app.api import api
 from app.models.dtos.user_dto import UserDTO
-from app.services.auth_service import basic_auth
+from app.services.auth_service import basic_auth, token_auth
 from app.services.user_service import UserService
 from app.services.utils import validate_dto, FlaskBootstrapError, get_logged_in_user
 
@@ -76,6 +76,47 @@ def login_user():
         description: Base64 encoded user password
         required: true
         type: string
+    responses:
+      200:
+        description: Login Successful
+      401:
+        description: Unauthorized, credentials are invalid
+      500:
+        description: Internal Server Error
+    """
+    try:
+        logged_in_user = get_logged_in_user()
+        return jsonify(logged_in_user.to_primitive()), 200
+    except FlaskBootstrapError as e:
+        current_app.logger.error(e.message)
+        return jsonify(e.error), e.status_code
+    except Exception as e:
+        error_message = f"Unhandled exception: {str(e)}"
+        current_app.logger.critical(error_message)
+        return jsonify({"errorMessage": error_message}), 500
+
+
+@api.route("/user/<int:user_id>", methods=["GET"])
+@token_auth.login_required
+def get_user(user_id: int):
+    """
+    Get user info
+    ---
+    tags:
+      - user
+    produces:
+      - application/json
+    parameters:
+      - in: header
+        name: Authorization
+        description: Base64 encoded user password
+        required: true
+        type: string
+      - in: path
+        name: user_id
+        description: User ID
+        type: int
+        example: 1
     responses:
       200:
         description: Login Successful
